f6ce5b8112e9b14eae11a7cd57d6077e336db8aa
sewoop <raku@hotmail.co.kr>
2020-10-18 03:04:15 +0900

    Refact - PC 연동 체크 및 수정 (#37)

--- deep/hanJamo.py
+++ deep/hanJamo.py
@@ -1,18 +1,25 @@
 from pprint import pprint
 import re
+import hgtk
+import numpy as np
+from hgtk.letter import decompose as decom_ltr
+from hgtk.text import decompose as decom_txt
 
 # 자모, 특수문자, 알파벳, 숫자 목록
-INITIAL = [char for char in "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ"]  # 19
-MIDIAL = [char for char in "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ"]  # 21
-FINAL = [char for char in "_ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ"]  # 28
-SINGLE = [char for char in "ㄱㄲㄳㄴㄵㄶㄷㄸㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅃㅄㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ"]  # 51
-ALPHABET = [char for char in "abcdefghijklmnopqrstuvwxyz"]  # 26
-NUMBER = [char for char in "0123456789"]  # 10
-SPECIAL = [char for char in "!@#$%^&*()_+~`-=[]{}\\|'\";:,./<>? ♥♡★☆\t\n"]  # 39
+INITIAL = ['ㄱ','ㄲ','ㄴ','ㄷ','ㄸ','ㄹ','ㅁ','ㅂ','ㅃ','ㅅ','ㅆ','ㅇ','ㅈ','ㅉ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ']  # 19
+MIDIAL = ['ㅏ','ㅐ','ㅑ','ㅒ','ㅓ','ㅔ','ㅕ','ㅖ','ㅗ','ㅘ','ㅙ','ㅚ','ㅛ','ㅜ','ㅝ','ㅞ','ㅟ','ㅠ','ㅡ','ㅢ','ㅣ']  # 21
+FINAL = ['ㄱ','ㄲ','ㄳ','ㄴ','ㄵ','ㄶ','ㄷ','ㄹ','ㄺ','ㄻ','ㄼ','ㄽ','ㄾ','ㄿ','ㅀ','ㅁ','ㅂ','ㅄ','ㅅ','ㅆ','ㅇ','ㅈ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ']  # 27 (null+)
+# SINGLE = ['ㄱ','ㄲ','ㄳ','ㄴ','ㄵ','ㄶ','ㄷ','ㄸ','ㄹ','ㄺ','ㄻ','ㄼ','ㄽ','ㄾ','ㄿ','ㅀ','ㅁ','ㅂ','ㅃ','ㅄ','ㅅ','ㅆ','ㅇ','ㅈ','ㅉ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ','ㅏ','ㅐ','ㅑ','ㅒ','ㅓ','ㅔ','ㅕ','ㅖ','ㅗ','ㅘ','ㅙ','ㅚ','ㅛ','ㅜ','ㅝ','ㅞ','ㅟ','ㅠ','ㅡ','ㅢ','ㅣ']  # 51
+# ALPHABET = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']  # 26
+# NUMBER = ['0','1','2','3','4','5','6','7','8','9']  # 10
+# SPECIAL = ['!','@','#','$','%','^','&','*','(',')','_','+','~','`','-','=','[',']','{','}','\\','|',"'",'"',';',':',',','.','/','<','>','?',' ','♥','♡','★','☆','\t','\n']  # 39
 
-TOTAL = INITIAL + MIDIAL + FINAL  # 68 (초성, 중성, 종성)
-TOTAL_ALL = TOTAL + SINGLE + ALPHABET + NUMBER + SPECIAL  # 194 (초,중,종,단일의미,알파벳,숫자,특수문자)
+TOTAL = INITIAL + MIDIAL + FINAL  # 67 (초성, 중성, 종성) => 종성이 없는 경우 포함 68
+# TOTAL_ALL = TOTAL + SINGLE + ALPHABET + NUMBER + SPECIAL  # 193 (초,중,종,단일의미,알파벳,숫자,특수문자) => 종성이 없는 경우 포함 194
 
+# *** 음성으로 나온 text에서 욕설을 찾아내는 것이므로 단일어, 알파벳, 특수문자, 숫자의 구분이 필요가 없다.
+
+# 이전 구현 내용
 def hang_to_jamo(string, end_char='_'):
     result = []
 
@@ -65,14 +72,35 @@ def jamo_to_hang(string):
 
     return result
 
+# hgtk를 활용
+def cho2onehot(s):
+    res = np.zeros(len(INITIAL))
+    if s in INITIAL:
+        res[INITIAL.index(s)]=1
+    return res
+
+def cwu2onehot(s):
+    res = np.zeros(len(MIDIAL))
+    if s in MIDIAL:
+        res[MIDIAL.index(s)]=1
+    return res
+
+def con2onehot(s):
+    res = np.zeros(len(FINAL))
+    if s in FINAL:
+        res[FINAL.index(s)]=1
+    return res
+
+def hang_onehot(s): # ex) s = '각'
+    z = decom_ltr(s)
+    res = np.zeros((len(TOTAL),3))
+    res[:len(INITIAL),0] = cho2onehot(z[0])
+    res[len(INITIAL):len(INITIAL)+len(MIDIAL),1] = cwu2onehot(z[1])
+    res[len(INITIAL)+len(MIDIAL):len(TOTAL),2] = con2onehot(z[2])
+    return res  # shape (67, 3)
+
 if __name__ == "__main__":
     # print(0xD7A3)  # 55203
     # print(0xAC00)  # 44032
-
-    text = input()
-    jamo = hang_to_jamo(text)   # 'ㅇㅏㄴㄴㅕㅇㅎㅏ_ㅅㅔ_ㅇㅛ_ ㅈㅔ_ ㅇㅣ_ㄹㅡㅁㅇㅡㄴ'
-
-    pprint(jamo)
-    pprint(jamo_to_hang(jamo))
-
-   
+    print(cho2onehot('ㄱ'))
+    print(hang_onehot('각'))
--- deep/preprocessing.py
+++ deep/preprocessing.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
 from pprint import pprint
-from hanJamo import hang_to_jamo
+from hanJamo import hang_onehot, hang_to_jamo, TOTAL
 
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 
 from keras.layers import Input, Embedding
 
+# 구
 def text_to_seq(texts):
     # text to lower
     texts = [s.lower() for s in texts]
@@ -19,11 +20,11 @@ def text_to_seq(texts):
     tk.fit_on_texts(texts)
 
     # -- classify char
+    alphabet = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`-=[]{}\\|'\";:,./<>? ♥♡★☆\t\n"
     # alphabet = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`-=[]{}\\|'\";:,./<>? ♥♡★☆\t\n"
-    # alphabet = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`-=[]{}\\|'\";:,./<>? ♥♡★☆\t\n"
-    korean = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ_ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ"
+    # korean = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ_ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ"
 
-    alphabet = korean
+    # alphabet = korean
 
     # 자모 dictionary
     char_dict = {}
@@ -71,19 +72,35 @@ def sequence_to_embedding(texts, input_size, embedding_size):
 
     return embedding_layer
 
+# 신
+def hang_to_embedding(texts):
+    vocab_size = len(TOTAL)
+
+    embedding_weights = []
+    # for text in texts:
+    #     embedding_weights.append(hang_onehot(text))
+    
+    pprint(hang_onehot(texts))
+    # embedding_layer = Embedding(vocab_size+1, 
+    #                             embedding_size, 
+    #                             input_length=input_size,
+    #                             weights=[embedding_weights])
+
+
 if __name__ == "__main__":
     # texts = ["wall st. bears claw back into the black (reuters)reuters - short-sellers, wall street's dwindling\\band of ultra-cynics, are seeing green again.",
     #    'carlyle looks toward commercial aerospace (reuters)reuters - private investment firm carlyle group,\\which has a reputation for making well-timed and occasionally\\controversial plays in the defense industry, has quietly placed\\its bets on another part of the market.',
     #    "oil and economy cloud stocks' outlook (reuters)reuters - soaring crude prices plus worries\\about the economy and the outlook for earnings are expected to\\hang over the stock market next week during the depth of the\\summer doldrums."]
-    texts = input()
-    texts = [hang_to_jamo(texts),]
 
-    input_size = 1014
-    embedding_size = 76
+    # texts = ['안녕하세요','반갑습니다','안녕히가세요']
+    texts = '안'
 
-    # test
-    embedding_size = 20
+    # input_size = 1014
+    # embedding_size = 76
 
-    embedding_layer = sequence_to_embedding(texts, input_size, embedding_size)
-    inputs = Input(shape=(input_size,), name='input', dtype='int64')
-    x = embedding_layer(inputs)
+    # # test
+    # embedding_size = 20
+
+    # embedding_layer = sequence_to_embedding(texts, input_size, embedding_size)
+
+    # embedding_layer = hang_to_embedding(texts)
