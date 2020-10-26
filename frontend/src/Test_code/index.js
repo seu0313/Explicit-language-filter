27238fe870f83332b8e54d5396eabde937d8b17c
sewoop <raku@hotmail.co.kr>
2020-10-27 02:34:56 +0900

    Update - Separate FE/BE files

--- frontend/src/Test_code/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Main } from './Main'
-export { default as Test } from './Test'
-export { default as NotFound } from './NotFound'
--- frontend/src/components/Loading/Loading.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress';
-
-export default function Loading() {
-  return <CircularProgress disableShrink />;
-}
