ea386c34f96e04053ec0521ad9a0b95229b315bb
seu0313 <raku@hotmail.co.kr>
2021-06-03 15:06:50 +0900

    chore: delete legacy code

--- client/src/a_legacyCode/components/Loading/Loading.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CircularProgress from "@material-ui/core/CircularProgress";
-
-const Loading = () => {
-  return <CircularProgress disableShrink />;
-};
-
-export default Loading;
