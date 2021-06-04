f2e2604dbde422ee812d132e196345de2c803757
seu0313 <raku@hotmail.co.kr>
2021-06-04 15:20:01 +0900

    feat: Create atoms - video thumbnail UI (#72)

---
+++ client/src/components/atoms/VideoThumbnail/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  width: 343px;
+`;
+export const VideoThumbnail = styled.img`
+  width: 343px;
+`;
