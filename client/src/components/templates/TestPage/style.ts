e0266621f775d5793500d628eda375ce9fd542de
seu0313 <raku@hotmail.co.kr>
2021-06-04 15:20:52 +0900

    feat: Create templates - test page UI (#72)

---
+++ client/src/components/templates/TestPage/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Elements = styled.div`
+  display: block;
+  align-items: center;
+  justify-content: center;
+  width: 343px;
+`;
