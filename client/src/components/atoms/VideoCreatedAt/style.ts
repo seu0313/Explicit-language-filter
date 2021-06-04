efd1ee8ea7b8f285ffdb67b3b7aa5992797ef134
seu0313 <raku@hotmail.co.kr>
2021-06-04 15:19:36 +0900

    feat: Create atoms - createdAt UI (#72)

---
+++ client/src/components/atoms/VideoCreatedAt/style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  width: 300px;
+`;
+export const VideoCreatedAt = styled.div`
+  font-size: 12px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  color: #aaaaaa;
+`;
