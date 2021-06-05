1e192cb34545244e266c698b4fabac54e7fb1bf8
sewoop <raku@hotmail.co.kr>
2021-06-05 18:33:03 +0900

    refact: global theme color & size (#79)

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -1,8 +1,27 @@
-const color = {};
-const size = {};
-const theme = {
+const color = {
+  primaryTextGray: "#111111",
+  primaryTextWhite: "#FFFFFF",
+  SecondaryTextGray: "#555555",
+  SecondaryTextWhite: "#B7B7B7",
+  TertiaryText: "#777777",
+  primaryAccentTextLight: "#638DFF",
+  primaryAccentTextDark: "#3B26FD",
+  button: {
+    Disabled: "#C6C6C6",
+    basicColor: "#00C300",
+    hover: "#00E000",
+    Press: "#00B300",
+  },
+};
+
+const size = {
+  mobile: "375px",
+  tablet: "768px",
+  labtop: "1024px",
+  desktop: "1440px",
+};
+
+export default {
   color,
   size,
 };
-
-export default theme;
